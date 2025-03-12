258106aa5533ab4094156228a4cf6e1e5a7cebf8
campingfire <132039104+hjg727@users.noreply.github.com>
2025-03-12 20:43:12 +0900

    [Gold V] Title: 꿀 따기, Time: 132 ms, Memory: 42660 KB, Score: 100 point -BaekjoonHub

--- 백준/Gold/21758. 꿀 따기/꿀 따기.py
+++ 백준/Gold/21758. 꿀 따기/꿀 따기.py
@@ -5,21 +5,25 @@ field = list(map(int, input().split()))
 ans = []
 
 def bee_bee_bucket(field):
-    max_val = -1\
-    
-    for i in range(1, N-1):
-        val = sum(field[1:N]) * 2 - (field[i]*2) - sum(field[1:i])
+    max_val = -1
+    tmp = [0] * N
+    for i in range(1, N):
+        tmp[i] = tmp[i-1] + field[i]
+
+    for i in range(1, N):
+        val = 2*tmp[N-1] - (field[i]*2) - tmp[i-1]
         max_val = max(max_val, val)
     
     return max_val
 
-def bee_bucket_bee(filed):
+def bee_bucket_bee(field):
     max_val = -1
+    tmp = [0] * N
+    for i in range(1, N-1):
+        tmp[i] = tmp[i-1] + field[i]
 
     for i in range(1, N-1):
-        val = field[i] * 2
-        val += sum(field[1:i])
-        val += sum(field[i+1:N-1])
+        val = tmp[N-2] + field[i]
         max_val = max(max_val, val)
     return max_val
 
