480ff22372e87b7a2c5d9ebfc49d88d4b78e6014
campingfire <132039104+hjg727@users.noreply.github.com>
2025-04-15 13:31:49 +0900

    [Silver II] Title: 사탕 게임, Time: 80 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/3085. 사탕 게임/사탕 게임.py
+++ 백준/Silver/3085. 사탕 게임/사탕 게임.py
@@ -1,63 +1,40 @@
-"""
-1. 해당 위치의 사탕과 행, 열에 인접한 위치의 사탕의 색이 다르다면 교환
-2. 교환된 상태에서 전체 맵에서 가장 긴 연속 부분 탐색(변경된 행과 열에 대해서만 탐색하고싶긴하다.)
-3. 다시 제자리로 바꾸고 해당위치는 방문처리
-"""
-def find_max_cnt(line):
-    max_cnt = 1
-    current = 1
-    previous = line[0]
-
-    for candy in line[1:]:
-        if candy == previous:
-            current += 1
-            max_cnt = max(max_cnt, current)
-        else:
-            previous = candy
-            current = 1
-    
-    return max_cnt
-
-def change_candy(board, x1, y1, x2, y2):
-    board[x1][y1], board[x2][y2] = board[x2][y2], board[x1][y1]
-
-    max_candy = 0
-    board_row = set([x1,x2])
-    for row in board_row:
-        max_candy = max(max_candy, find_max_cnt(board[row]))
-    
-    board_col = set([y1,y2])
-    for col in board_col:
-        lines = [board[row][col] for row in range(N)]
-        max_candy = max(max_candy, find_max_cnt(lines))
-    
-    board[x1][y1], board[x2][y2] = board[x2][y2], board[x1][y1]
-    return max_candy
-
-def check_change(board, x, y):
-    max_candy = -1e9
-    visited[x][y] = True
-
-    for i in range(4):
-        nx, ny = x + dx[i], y + dy[i]
-
-        if 0 <= nx < N and 0 <= ny <N and not visited[nx][ny]:
-            max_candy = max(max_candy,change_candy(board, x, y, nx, ny))
-    
-    return max_candy
-
 N = int(input())
 
-board = [list(input().rstrip()) for _ in range(N)]
-visited = [[False] * N for _ in range(N)]
-dx = [-1, 1, 0, 0]
-dy = [0, 0, -1, 1]
-res = []
+field = []
+for _ in range(N):
+    field.append(list(input().rstrip()))
+res = 0
+def find_max_cnt(line):
+    previous = line[0]
+    cnt = 1
+    curr = 1
+    for candy in line[1:]:
+        if candy == previous:
+            curr += 1
+            cnt = max(cnt, curr)
+        else:
+            previous = candy
+            curr = 1
+    return cnt
 
+def change_candy(field, x1, y1, x2, y2):
+    field[x1][y1], field[x2][y2] = field[x2][y2], field[x1][y1]
+    max_candy = 0
+    for row in set([x1, x2]):
+        max_candy = max(max_candy, find_max_cnt(field[row]))
+    
+    for col in set([y1, y2]):
+        lines = [field[row][col] for row in range(N)]
+        max_candy = max(max_candy, find_max_cnt(lines))
+    field[x1][y1], field[x2][y2] = field[x2][y2], field[x1][y1]
+
+    return max_candy
 
 for i in range(N):
     for j in range(N):
-        if not visited[i][j]:
-            res.append((check_change(board, i, j)))
+        if j + 1 < N:
+            res= max(res, change_candy(field, i, j, i, j+1))
+        if i + 1 < N:
+            res= max(res, change_candy(field, i, j, i+1, j))
 
-print(max(res))
+print(res)
