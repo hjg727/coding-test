125444124da5a5d4225cb6b9400e720f16d92b95
campingfire(홍정기) <tomy980050@gmail.com>
2025-09-19 20:02:14 +0900

    [Silver III] Title: N과 M (6), Time: 44 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/15655. N과 M （6）/N과 M （6）.py
@@ -0,0 +1,20 @@
+N, M = map(int, input().split())
+num = sorted(list(map(int, input().split())))
+res = []
+def bt(start, path):
+    if len(path) == M:
+        res.append(path[:])
+        return
+    if start >= len(num):
+        return
+    
+    for i in range(start, len(num)):
+        # if i not in path:
+        path.append(num[i])
+        bt(i+1, path)
+        path.pop()
+
+bt(0, [])
+
+for num in sorted(res):
+    print(' '.join(map(str, num)))
