3ab064625bcd51d6dde7ce07610704d22ea39ccd
campingfire(홍정기) <tomy980050@gmail.com>
2025-09-24 22:13:37 +0900

    [Silver I] Title: NM과 K (1), Time: 2652 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/18290. NM과 K （1）/NM과 K （1）.py
+++ 백준/Silver/18290. NM과 K （1）/NM과 K （1）.py
@@ -1,7 +1,7 @@
 N, M, K = map(int, input().split())
 
 field = [list(map(int, input().split())) for _ in range(N)]
-result = float('-inf')  # 변경 1: 음수 값도 고려하여 -inf로 초기화
+result = float('-inf')
 
 visited = [[False] * M for _ in range(N)]
 
@@ -10,7 +10,7 @@ def validate(x, y):
         return False
     if visited[x][y]:
         return False
-    directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+    directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]  # 상, 하, 좌, 우
 
     for dx, dy in directions:
         nx, ny = x + dx, y + dy
@@ -21,26 +21,20 @@ def validate(x, y):
     return True
 
 
-def bt(cnt, start_idx, sum_num):
+def bt(cnt, start_x, start_y, sum_num):
     global result
     if cnt == K:
         result = max(result, sum_num)
         return
+    
+    for i in range(start_x, N):
+        start_j = start_y if start_x == i else 0
+        for j in range(start_j, M):
+            if validate(i, j):
+                visited[i][j] = True
+                bt(cnt+1, i, j, sum_num + field[i][j])
+                visited[i][j] = False
 
-    remaining_cells = N * M - start_idx
-    needed_cells = K - cnt
-    if remaining_cells < needed_cells:
-        return
-
-    for idx in range(start_idx, N * M):
-        i = idx // M  # 행
-        j = idx % M   # 열
-        
-        if validate(i, j):
-            visited[i][j] = True
-            bt(cnt + 1, idx + 1, sum_num + field[i][j])
-            visited[i][j] = False
-
-bt(0, 0, 0)
+bt(0, 0, 0, 0)
 
 print(result)
