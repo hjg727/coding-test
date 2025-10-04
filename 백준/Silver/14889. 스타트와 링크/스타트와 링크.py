2b20c72ea66086abd5ce02d98a2712983dec1301
campingfire(홍정기) <tomy980050@gmail.com>
2025-10-04 19:07:22 +0900

    [Silver I] Title: 스타트와 링크, Time: 312 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/14889. 스타트와 링크/스타트와 링크.py
+++ 백준/Silver/14889. 스타트와 링크/스타트와 링크.py
@@ -1,28 +1,30 @@
 N = int(input())
-ans = 10000000000
 S = [list(map(int, input().split())) for _ in range(N)]
-check = [False for _ in range(N)]
 
-def calc(start, link):
-    for i in range(N):
-        for j in range(N):
-            if check[i] and check[j]:
-                start += S[i][j]
-            if not check [i] and not check[j]:
-                link += S[i][j]
-    return abs(start - link)
-def DFS(count, cand):
-    global ans
-    if count == N//2:
-        temp = calc(0, 0)
-        if ans > temp:
-            ans = temp
+res = 1e9
+total = 0
+powers = [0] * N
+for i in range(N):
+    for j in range(N):
+        total += S[i][j]
+        powers[i] += S[i][j]
+        powers[j] += S[i][j]
+
+def bt(path, start):
+    global res
+    global total
+    if len(path) == N//2:
+        a=0
+        for i in path:
+            a += powers[i]
+        res = min(res, abs(total-a))
         return
-    else:
-        for i in range(cand, N):
-            check[i] = True
-            DFS(count + 1, i + 1)
-            check[i] = False
-
-DFS(0, 1)
-print(ans)
+    if res == 0:
+        return
+    for i in range(start, N):
+        if i not in path:
+            path.append(i)
+            bt(path, i+1)
+            path.pop()
+bt([], 0)
+print(res)
