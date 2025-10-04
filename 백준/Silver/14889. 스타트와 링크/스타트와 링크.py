8f4584225053942fbf773b7b830f0c4c6db13a88
campingfire(홍정기) <tomy980050@gmail.com>
2025-10-04 19:14:15 +0900

    [Silver I] Title: 스타트와 링크, Time: 220 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/14889. 스타트와 링크/스타트와 링크.py
+++ 백준/Silver/14889. 스타트와 링크/스타트와 링크.py
@@ -1,6 +1,6 @@
 N = int(input())
 S = [list(map(int, input().split())) for _ in range(N)]
-
+from itertools import combinations
 res = 1e9
 total = 0
 powers = [0] * N
@@ -10,21 +10,9 @@ for i in range(N):
         powers[i] += S[i][j]
         powers[j] += S[i][j]
 
-def bt(path, start):
-    global res
-    global total
-    if len(path) == N//2:
-        a=0
-        for i in path:
-            a += powers[i]
-        res = min(res, abs(total-a))
-        return
+for team in combinations(range(N), N//2):
+    a = sum(powers[i] for i in team)
+    res = min(res, abs(total-a))
     if res == 0:
-        return
-    for i in range(start, N):
-        if i not in path:
-            path.append(i)
-            bt(path, i+1)
-            path.pop()
-bt([], 0)
+        break
 print(res)
