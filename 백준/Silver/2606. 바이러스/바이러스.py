f840652f29342333a68a9ffbbcde19e91a749935
campingfire(홍정기) <tomy980050@gmail.com>
2025-10-17 18:39:45 +0900

    [Silver III] Title: 바이러스, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/2606. 바이러스/바이러스.py
@@ -0,0 +1,25 @@
+import sys
+sys.setrecursionlimit(10**6)
+n = int(input())
+computers = [[] for _ in range(n)]
+tmp = int(input())
+visited = [False] * n
+
+for _ in range(tmp):
+    a, b = map(int, input().split())
+    computers[a-1].append(b-1)
+    computers[b-1].append(a-1)
+res = 0
+
+def dfs(node):
+    global res
+    visited[node] = True
+
+    for i in computers[node]:
+        if not visited[i]:
+            if node in computers[i]:
+                dfs(i)
+                res += 1
+dfs(0)
+
+print(res)
