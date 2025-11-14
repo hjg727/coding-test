d686ffeaa524f672a81c4c23b36b7cd4ed13b447
campingfire(홍정기) <tomy980050@gmail.com>
2025-11-14 22:13:01 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.py
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.py
@@ -1,12 +1,12 @@
-T = int(input())
-dp = [0] * 12
+t = int(input())
+dp = [0]*12
+
 dp[1] = 1
 dp[2] = 2
 dp[3] = 4
-
 for i in range(4, 12):
-    dp[i] = sum(dp[i-3:i])
+    dp[i] = dp[i-1]+dp[i-2]+dp[i-3]
 
-for _ in range(T):
-    N = int(input())
-    print(dp[N])
+for _ in range(t):
+    n = int(input())
+    print(dp[n])
