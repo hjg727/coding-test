8a0135d1e5e9b433f7a6883a9b68eb63d386f1be
campingfire(홍정기) <tomy980050@gmail.com>
2026-01-05 21:37:12 +0900

    [Silver III] Title: 수열, Time: 28 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Silver/2559. 수열/수열.cc
+++ 백준/Silver/2559. 수열/수열.cc
@@ -1,27 +1,29 @@
 #include <bits/stdc++.h>
 using namespace std;
-int N, K, res;
-//res가 0여서 -값이 최댓값보다 0이 나옴
-vector<int> input;
+
 int main(){
+    
+    int N, K;
     cin >> N >> K;
-
+    
+    vector<int> input(N);
+    
     for(int i = 0; i<N; i++){
-        int x;
-        cin >> x;
-        input.push_back(x);
-    }
-    for(int i = 0; i<K; i++){
-        res += input[i];
+        cin >> input[i];
     }
 
-    for(int i = 1; i<=N-K; i++){
-        int tmp = 0;
-        for(int j = i; j<i+K; j++){
-            tmp += input[j];
-        }
-        res = max(tmp, res);
+    int sum = 0;
+    for(int i = 0; i<K; i++){
+        sum += input[i];
     }
+    int res = sum;
+
+    for(int i = K; i<N; i++){
+        sum += input[i];
+        sum -= input[i-K];
+        res = max(res, sum);
+    }
+    
     cout << res;
     return 0;
 }
