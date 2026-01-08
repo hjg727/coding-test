55d521c0dead1190e207fcf27b892c852bd766f0
campingfire(홍정기) <tomy980050@gmail.com>
2026-01-08 20:20:16 +0900

    [Silver I] Title: 곱셈, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/1629. 곱셈/곱셈.cc
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+ll a, b, c;
+
+ll go(ll a, ll b){
+    if(b==1) return a%c;
+
+    ll ret = go(a, b/2);
+    ret = (ret*ret)%c;
+    if(b%2) ret = (ret*a)%c;
+    return ret;
+}
+
+int main() {
+    cin >> a >> b >> c;
+    ll ret = go(a,b);
+    cout << ret << '\n';
+    return 0;
+}
