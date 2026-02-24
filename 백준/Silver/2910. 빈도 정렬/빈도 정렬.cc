4d3a92cf5277faab6280be78601242694fd266f2
campingfire(홍정기) <tomy980050@gmail.com>
2026-02-24 17:38:33 +0900

    [Silver III] Title: 빈도 정렬, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Silver/2910. 빈도 정렬/빈도 정렬.cc
+++ 백준/Silver/2910. 빈도 정렬/빈도 정렬.cc
@@ -1,30 +1,35 @@
 #include <bits/stdc++.h>
 using namespace std;
-int n, c, a[1004];
-vector<pair<int, int>> v;
-map<int, int> mp, mp_first;//mp_first 는 숫자, 순서
-bool cmp(pair<int, int> a, pair<int,int>b){
+
+int n, c;
+vector<pair<int,int>> v;
+map<int, int> mp, mp_first;// mp는 나온 횟수, mp_first는 처음 나온 위치
+
+bool cmp(pair<int, int> a, pair<int, int> b){
     if(a.first == b.first){
         return mp_first[a.second] < mp_first[b.second];
     }
-    return a.first>b.first;
+    return a.first > b.first;
 }
-int main(){
+int main() {
     cin >> n >> c;
     int tmp;
-    for(int i =0; i<n; i++){
+    for(int i = 0; i<n; i++){
         cin >> tmp; mp[tmp]++;
-        if(mp_first[tmp] == 0) mp_first[tmp] = i+1;
+        if(mp_first[tmp]==0) mp_first[tmp]=i+1;
     }
-    for(auto it : mp){
+
+    for(auto it: mp){
         v.push_back({it.second, it.first});
-        //갯수, 숫자
     }
+
     sort(v.begin(), v.end(), cmp);
+
     for(auto i : v){
         for(int j =0; j<i.first; j++){
             cout << i.second << ' ';
         }
     }
-    
+
+    return 0;
 }
