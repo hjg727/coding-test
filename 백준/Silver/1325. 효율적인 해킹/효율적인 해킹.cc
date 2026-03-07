7e22ff3a8f59648adad3ecf9a11987d80289c434
campingfire(홍정기) <tomy980050@gmail.com>
2026-03-07 16:59:17 +0900

    [Silver I] Title: 효율적인 해킹, Time: 2328 ms, Memory: 3540 KB -BaekjoonHub

--- 백준/Silver/1325. 효율적인 해킹/효율적인 해킹.cc
+++ 백준/Silver/1325. 효율적인 해킹/효율적인 해킹.cc
@@ -1,37 +1,31 @@
-//신뢰하는 관계와 신뢰하지않는 관계
-//신뢰 -> a가b를 신뢰하면 b->a ㄱㄴ 
 #include <bits/stdc++.h>
 using namespace std;
 
-int n, m;
-int mx;
-vector<int> adj[10001];
+vector<int> v[10001];
+int dp[10001], mx, visited[10001], n, m, a, b;
 
-int dfs(int node,vector<bool>& visited){
-    visited[node] = true;
-    int cnt = 1;
-    for(int child : adj[node]){
-        if(!visited[child]) cnt += dfs(child, visited);
+int dfs(int here){
+    visited[here] = 1;
+    int ret = 1;
+    for(int there : v[here]){
+        if(!visited[there]) ret += dfs(there);
     }
-    return cnt;
+    return ret;
 }
+
 int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
     cin >> n >> m;
-    vector<int> cnt;
-    for(int i =0; i<m; i++){
-        int a, b;
+    while(m--){
         cin >> a >> b;
-        adj[b].push_back(a);
+        v[b].push_back(a);
     }
-
     for(int i = 1; i<=n; i++){
-        vector<bool> visited(n+1, false);
-        cnt.push_back(dfs(i, visited));
+        memset(visited, 0, sizeof(visited));
+        dp[i] = dfs(i);
+        mx = max(dp[i], mx);
     }
-    mx = *max_element(cnt.begin(), cnt.end());
-
-    for(int i = 1; i<=n; i++){
-        if(cnt[i-1] == mx) cout << i << ' ';
-    }
+    for(int i = 1; i<=n; i++) if(mx==dp[i]) cout << i<< ' ';
     return 0;
 }
